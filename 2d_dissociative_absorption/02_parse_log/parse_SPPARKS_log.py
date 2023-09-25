fc1511bdd961cbcd2e70823a08c9f6e2ee123f26
Changho Kim <ckim103@ucmerced.edu>
2023-09-25 15:14:07 -0700

    2d_dissociative_absorption/02_parse_log/parse_SPPARKS_log.py: only parsing part included

--- 2d_dissociative_absorption/02_parse_log/parse_SPPARKS_log.py
+++ 2d_dissociative_absorption/02_parse_log/parse_SPPARKS_log.py
@@ -6,11 +6,11 @@ import json
 import re
 
 logdir = "../log"
-datadir = "../log/computed_data"
+resdir = "../res"
 
 # Create the folder to save computed data if it doesn't exist
-if not os.path.exists(datadir):
-    os.makedirs(datadir)
+if not os.path.exists(resdir):
+    os.makedirs(resdir)
 
 #read input variables from json file
 with open(os.path.join(logdir, 'variables.txt')) as f:
@@ -23,26 +23,17 @@ Nruns = var_data["Nruns"]
 Nstep = var_data["Nstep"]
 deltat = var_data["deltat"]
 
-print("Total of " + str(Nruns) + " log files detected.")
-
-maxtau = int(min(Nstep * 0.9, 100000))  #maxtau will be set to 90% of Nstep, 250 maximum
-theta = 1 / (1 + math.sqrt(rd2 / ra2))
-
-# initialize arrays
-all_corrs = []
-average_corrs = np.zeros(maxtau - 1)
-variances = np.zeros(maxtau - 1)
-
 #find log files
 log_files = [f for f in os.listdir(logdir) if f.startswith('log') and f.endswith('.spparks')]
 
-
+print("** a total of %d log files detected" % (len(log_files)))
 
 # Loop through all log files
 for i in range(1, Nruns+1):
     x_values = []
     y_values = []
     filename = os.path.join(logdir, "log{}.spparks".format(i))
+    print("reading %s" % filename)
 
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -69,37 +60,3 @@ for i in range(1, Nruns+1):
                 x_values.append(float(save_previous_line.group(1)))
                 y_values.append(float(save_previous_line.group(7)) / total_sites)
                 previous_line_is_data = False
-
-    corrs = []
-    new_values = np.array(y_values)
-    new_values -= theta
-    for tau in range(1, maxtau):
-        new_x = new_values[:-tau]
-        new_y = new_values[tau:]
-        corrs.append(np.correlate(new_x, new_y, mode='valid')[0])
-        #corrs.append(np.corrcoef(new_x, new_y)[0, 1])
-
-    #save individual conrrelations
-    individual_data_to_save = np.column_stack((range(1, maxtau), corrs))
-    np.savetxt(os.path.join(datadir, "correlation{}.txt".format(i)), individual_data_to_save, fmt="%d %f", header="i correlation")
-    all_corrs.append(corrs)
-    average_corrs += np.array(corrs)
-
-
-average_corrs /= Nruns
-
-
-# Calculate the variance for each point
-for tau in range(maxtau - 1):
-    mean_value = average_corrs[tau]
-    #only calculate variance when num_files is more than one
-    if (Nruns > 1):
-        variance = sum((np.array([corrs[tau] for corrs in all_corrs]) - mean_value)**2) / (Nruns - 1)
-        variances[tau] = variance
-    else:
-        variances[tau] = 0
-
-# Save the average_corrs and variances to a file
-data_to_save = np.column_stack((range(1, maxtau), average_corrs, variances))
-np.savetxt(os.path.join(datadir, "average_correlation.txt"), data_to_save, fmt="%d %f %f", header="i average_correlation variance")
-print("Finished writing correlation.txt files in ../log/computed_data")
