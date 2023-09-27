b258c84ec933df689e66e84785f7071dac03c1fe
Patrick Park <3125patrick@gmail.com>
2023-09-27 12:04:18 -0700

    compute_av_correation now also computes variances

--- 2d_dissociative_absorption/03_stats/compute_av_correlation.py
+++ 2d_dissociative_absorption/03_stats/compute_av_correlation.py
@@ -1,36 +1,41 @@
 import numpy as np
 import os
+import json
 
+logdir = "../log"
 corrdir = "../correlation"
-output_file = os.path.join(corrdir, "average_correlation.txt")
 
-# Find correlation files
-correlation_files = [f for f in os.listdir(corrdir) if f.startswith('correlation') and f.endswith('.txt')]
+# Ensure the directory exists
+if not os.path.exists(corrdir):
+    os.makedirs(corrdir)
 
-# Initialize sum and count for averaging
-correlation_sums = None
-file_count = 0
+# Read input variables from the JSON file
+with open(os.path.join(logdir, 'variables.txt')) as f:
+    var_data = json.load(f)
 
-# Loop through each correlation file
-for file in correlation_files:
-    filepath = os.path.join(corrdir, file)
-    data = np.loadtxt(filepath)
+Nruns = var_data["Nruns"]
+Nstep = var_data["Nstep"]
+maxtau = int(min(Nstep * 0.9, 100000))
 
-    # If sums array is not initialized, create one with the same shape as data
-    if correlation_sums is None:
-        correlation_sums = np.zeros(data.shape)
+# Read all correlations once and store in a list
+all_corrs = [np.loadtxt(os.path.join(corrdir, "correlation" + str(i) + ".txt"))[:, 1] for i in range(1, Nruns + 1)]
 
-    # Add the correlations to the sums array (assuming correlations are in the second column)
-    correlation_sums[:, 1] += data[:, 1]
-    file_count += 1
+# Calculate the average correlations
+average_corrs = np.mean(all_corrs, axis=0)
 
-# Calculate the average
-if file_count > 0:
-    correlation_sums[:, 1] /= file_count
+# Calculate the variance for each point
 
-# Save the average correlations to a file
-header_str = "i average_correlation"
-np.savetxt(output_file, correlation_sums, fmt="%d %f", header=header_str)
+variances = []
+if Nruns > 1:
+    for tau in range(maxtau - 1):
+        mean_value = average_corrs[tau]
+        variance = sum([(corrs[tau] - mean_value)**2 for corrs in all_corrs]) / (Nruns - 1)
+        variances.append(variance)
+else:
+    variances = np.zeros(maxtau - 1)
 
-print("Finished writing average correlations to {}".format(output_file))
+# Save the average_corrs and variances to a file
+data_to_save = np.column_stack((range(1, maxtau), average_corrs, variances))
+np.savetxt(os.path.join(corrdir, "average_correlation.txt"), data_to_save, fmt="%d %f %f", header="i average_correlation variance")
+print("Finished writing average_correlation.txt file in {}".format(corrdir))
 
