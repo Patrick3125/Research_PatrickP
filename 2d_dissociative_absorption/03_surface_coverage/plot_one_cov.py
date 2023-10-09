6b6e0124de8cf463e74021db63f56a98e70a172a
Patrick Park <3125patrick@gmail.com>
2023-10-09 14:47:31 -0700

    fixed plot_one_cov.py

--- 2d_dissociative_absorption/03_surface_coverage/plot_one_cov.py
+++ 2d_dissociative_absorption/03_surface_coverage/plot_one_cov.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = []
 y_values = []
 
-with open("../res/res1.txt", "r") as f:
+with open("../res/data1.txt", "r") as f:
     lines = f.readlines()[1:]
     for line in lines:    
         x_values.append(float(line.split()[0]))
