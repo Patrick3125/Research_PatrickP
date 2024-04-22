ef952cb487d3327d028eed367aed446ac83766ef
Patrick Park <3125patrick@gmail.com>
2024-04-21 22:48:27 -0700

    added plot_theta for periodic 2d and terminal 2d

--- terminal_2d_disads/03_surface_coverage/plot_theta.py
+++ terminal_2d_disads/03_surface_coverage/plot_theta.py
@@ -3,48 +3,69 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import math
 import numpy as np
-# Redefine symbols
+import json
+import os
+# define symbols
 m, j, k = symbols('m j k', integer=True)
 k_value = 5
 
-# Create lists to store the results
-theta_2m_plus_1_results = []
-theta_from_res_results = []
-product_values = []  # This will store the product of x and y
-theta_means = []  # Store the mean of theta values
+theta_analytical = []
+theta_simulation = []
+num_sites = []
+theta_means = []
 theta_errors = []
+theta = -1
 is_bipartite=[]
 
-N_values = list(range(3, 101, 1))
-for x in range(3, 10):
-    for y in range(3, 11):
-        product = x * y
-        product_values.append(product)
+#reading how many log files there are 
+log_folders = [item for item in os.listdir('../') if os.path.isdir(os.path.join('../', item)) and item.startswith('log_') and item.count('_') == 2]
+x_vals = []
+y_vals = []
+for folder in log_folders:
+    parts = folder.split('_')
+    if len(parts) == 3:
+        try:
+            x = int(parts[1])
+            y = int(parts[2])
+            x_vals.append(x)
+            y_vals.append(y)
+        except ValueError:
+            continue
+for x in x_vals:
+    for y in y_vals:
+        N = x * y
+        num_sites.append(N)
+        # Always bipartite
         is_bipartite.append(True)
-        N = product  # Assuming N is the product of x and y
-        if N % 2 == 1:
-            m_value = (N - 1) / 2  # Use integer division for m_value
-            numerator_sum = Sum((m - j) * binomial(m+1, (m-j))*binomial(m, j) / k**(m-j), (j, 0, m))
-            denominator_sum = Sum(binomial(m+1, (m-j)) * binomial(m,j) / k**(m-j), (j, 0, m))
-            theta_2m_plus_1_expr = 2 / (m*2+1) * (numerator_sum / denominator_sum)
 
-#        numerator_sum = Sum((m - j) * binomial(2*m+1, 2*(m-j)) / k**(m-j), (j, 0, m))
-#        denominator_sum = Sum(binomial(2*m+1, 2*(m-j)) / k**(m-j), (j, 0, m))
-#        theta_2m_plus_1_expr = 2 / (m*2+1) * (numerator_sum / denominator_sum)
-        #numerator_sum = Sum(binomial(2*m, 2*j+1) * k**j, (j, 0, m-1))
-        #denominator_sum = Sum(binomial(2*m+1, 2*j+1) * k**j, (j, 0, m))
-        #theta_2m_plus_1_expr = (numerator_sum / denominator_sum)
+        # Use different analytical equation depending on oddity of x and y
+        if x %2 ==0 or y %2 ==0:
+            m_value = N / 2  
+            numerator_sum = Sum(binomial(m, j) * binomial(m-1,j) * k**j, (j, 0, m-1))
+            denominator_sum = Sum(binomial(m, j)**2 * k**j, (j, 0, m))
+            theta_analytical_expr =(numerator_sum / denominator_sum)
 
-        else:
-            m_value = N / 2  # Use integer division for m_value
-            numerator_sum = Sum((m - j) * binomial(m, j)**2 / k**(m-j), (j, 0, m))
-            denominator_sum = Sum(binomial(m, j)**2 / k**(m-j), (j, 0, m))
-            theta_2m_plus_1_expr = 2 / (m*2) * (numerator_sum / denominator_sum)
+        elif x %2 ==1 and y %2 ==1:
+            m_value = (N - 1)  / 2  
+            numerator_sum = Sum(binomial(m+1, j+1) *binomial(m,j)* (m-j)* k**j, (j, 0, m-1))
+            denominator_sum = Sum(binomial(m+1, j+1)*binomial(m,j) * k**j, (j, 0, m))
+            theta_analytical_expr = 2 / (2*m+1)*(numerator_sum / denominator_sum)
 
-        theta_2m_plus_1_simplified = theta_2m_plus_1_expr.subs({m: m_value, k: k_value})
-        theta_2m_plus_1 = theta_2m_plus_1_simplified.doit().evalf()
-        theta_2m_plus_1_results.append(theta_2m_plus_1)
-        
+
+        # Get k value from json file
+        with open('../log_{}_{}'.format(x, y) + '/variables.txt') as f:
+            data = json.load(f)
+            ra2 = data['ra2']
+            rd2 = data['rd2']
+            k_value = rd2/ra2
+            theta = 1/(1+math.sqrt(k_value))
+
+
+        theta_analytical_simplified = theta_analytical_expr.subs({m: m_value, k: k_value})
+        theta_analytical_temp = theta_analytical_simplified.doit().evalf()
+        theta_analytical.append(theta_analytical_temp)
+
+        # Compute the error bar and average from simulation data
         with open('../res_{}_{}'.format(x, y) + '/average_surface_coverage.txt') as f:
             f.readline()  # Skip the first row
             theta_values = [float(line) for line in f]  # Assuming the second column is theta
@@ -56,80 +77,107 @@ for x in range(3, 10):
             theta_means.append(theta_mean)
             theta_errors.append(standard_error)
  
-theta_errors_2sigma = [error * 2 for error in theta_errors]
+theta_errors_3sigma = [error * 3 for error in theta_errors]
 
-print(theta_errors)
-theta = 1/(1+math.sqrt(5))
-# Plot the graph
-plt.figure(figsize=(8, 4))
+plt.figure(figsize=(4.5, 3))
 
-bipartite_product_values = []
-bipartite_theta_means = []
-bipartite_theta_errors= []
-nonbipartite_product_values = []
-nonbipartite_theta_means = []
-nonbipartite_theta_errors= []
+# Initializing lists for storing sorted data
+bp_sites = []  
+bp_means = []  
+bp_errors = [] 
+nbp_sites = [] 
+nbp_means = []  
+nbp_errors = [] 
 
-for i in range(len(product_values)):
+# Sort num_sites in numerical order for plotting
+for i in range(len(num_sites)):
     if is_bipartite[i]:
-        bipartite_product_values.append(product_values[i])
-        bipartite_theta_means.append(theta_means[i])
-        bipartite_theta_errors.append(theta_errors_2sigma[i])
+        bp_sites.append(num_sites[i])
+        bp_means.append(theta_means[i])
+        bp_errors.append(theta_errors_3sigma[i])
     else:
-        nonbipartite_product_values.append(product_values[i])
-        nonbipartite_theta_means.append(theta_means[i])
-        nonbipartite_theta_errors.append(theta_errors_2sigma[i])
-               
-zipped_bipartite = zip(bipartite_product_values, bipartite_theta_means, bipartite_theta_errors)
-sorted_bipartite = sorted(zipped_bipartite)
-bipartite_product_values, bipartite_theta_means, bipartite_theta_errors = zip(*sorted_bipartite)
+        nbp_sites.append(num_sites[i])
+        nbp_means.append(theta_means[i])
+        nbp_errors.append(theta_errors_3sigma[i])
+
+# Sort the bipartite lists
+if bp_sites:
+    zipped_bp = zip(bp_sites, bp_means, bp_errors)
+    sorted_bp = sorted(zipped_bp)
+    bp_sites, bp_means, bp_errors = zip(*sorted_bp)
+else:
+    bp_sites, bp_means, bp_errors = ([],[],[])
 
 # Sort the non-bipartite lists
-#zipped_nonbipartite = zip(nonbipartite_product_values, nonbipartite_theta_means, nonbipartite_theta_errors)
-#sorted_nonbipartite = sorted(zipped_nonbipartite)
-#nonbipartite_product_values, nonbipartite_theta_means, nonbipartite_theta_errors = zip(*sorted_nonbipartite)
+if nbp_sites:
+    zipped_nbp = zip(nbp_sites, nbp_means, nbp_errors)
+    sorted_nbp = sorted(zipped_nbp)
+    nbp_sites, nbp_means, nbp_errors = zip(*sorted_nbp)
+else:
+    nbp_sites, nbp_means, nbp_errors = ([], [], [])
 
 # Convert zipped lists back to lists
-bipartite_product_values = list(bipartite_product_values)
-bipartite_theta_means = list(bipartite_theta_means)
-bipartite_theta_errors = list(bipartite_theta_errors)
+bp_sites = list(bp_sites)
+bp_means = list(bp_means)
+bp_errors = list(bp_errors)
 
-#nonbipartite_product_values = list(nonbipartite_product_values)
-#nonbipartite_theta_means = list(nonbipartite_theta_means)
-#nonbipartite_theta_errors = list(nonbipartite_theta_errors)
+nbp_sites = list(nbp_sites)
+nbp_means = list(nbp_means)
+nbp_errors = list(nbp_errors)
  
-plt.errorbar(nonbipartite_product_values, nonbipartite_theta_means, yerr=nonbipartite_theta_errors, fmt='x-', label='Theta for Non-bipartite', ecolor='red', color='orange')
 
-# Plot for bipartite values
-plt.errorbar(bipartite_product_values, bipartite_theta_means, yerr=bipartite_theta_errors, fmt='x-', label='Theta for Bipartite', ecolor='blue', color='green')
+# lines for non-bipartite
+plt.plot(nbp_sites, nbp_means, '-', color='green', linewidth=1, zorder=10)
+
+# Error bars for non-bipartite
+plt.errorbar(nbp_sites,
+             nbp_means,
+             yerr=nbp_errors,
+             fmt='none', ecolor='red', zorder=20)
+
+# Plot lines for bipartite
+plt.plot(bp_sites, bp_means, '-', color='blue', linewidth=1, zorder=10)
+
+# Error bars for bipartite
+plt.errorbar(bp_sites,
+             bp_means,
+             yerr=bp_errors,
+             fmt='none', ecolor='red', zorder=20)
+
+#empty graph for labeling
+plt.errorbar(0,0, yerr = 0, color = 'green', ecolor='red', label = r'Non-bipartite lattice with $N$ values')
+plt.errorbar(0,0, yerr = 0, color = 'blue', ecolor='red', label = r'Bipartite lattice with $N$ values')
 
 
 #plt.scatter(product_values, theta_from_res_results, label='Theta from Simulation', marker='x')
-plt.scatter(product_values, theta_2m_plus_1_results, label='Theoretical Theta', marker='o',facecolors='none',  edgecolors = 'blue')
-
-plt.axhline(y=theta, color='purple', linestyle='--', label='theta')
+plt.scatter(num_sites, theta_analytical, label=r'Theoretical $\overline{\theta}_N$', marker='o',facecolors='none',  edgecolors = 'blue')
 
 plt.gca().xaxis.set_major_locator(MaxNLocator(6))
 plt.gca().yaxis.set_major_locator(MaxNLocator(6))
 
-plt.xlim(0, int(max(product_values)*1.1))
+plt.axhline(y=theta, color='black', linestyle=':')
+plt.text(x=110,
+         y=theta+0.003,
+         s=r'$\overline{\theta}_\infty$',
+         fontsize=12)
 
-# Set labels for both axes. Assuming you want to manually set labels at specific positions
-# For demonstration, setting labels at the first, middle, and last ticks
+plt.xlim(0, 110)
+plt.ylim(0.23, 0.33)
+
 x_ticks = plt.gca().get_xticks()
 y_ticks = plt.gca().get_yticks()
-print(y_ticks)
 
 plt.gca().set_xticklabels(['{:.0f}'.format(x_ticks[0]), '', '{:.0f}'.format(x_ticks[2]), '', '{:.0f}'.format(x_ticks[4])])
 plt.gca().set_yticklabels(['{:.2f}'.format(y_ticks[0]), '{:.2f}'.format(y_ticks[1]), '', '{:.2f}'.format(y_ticks[3]), '', '{:.2f}'.format(y_ticks[5])])
 
-plt.rcParams['font.size'] = 18  # You can adjust this size as needed
+plt.rcParams['font.size'] = 12
 
-
-plt.xlabel('Number of sites')
-plt.ylabel('Theta')
-plt.title('Theta vs Number of sites for 2d periodic case')
+plt.xlabel(r'Number of sites $N$', fontsize = 12)
+plt.ylabel(r'$\overline{\theta}_N$  ', rotation='horizontal', fontsize = 12, labelpad=7)
 plt.legend(loc='lower right')
-#plt.legend(fontsize='large')  # You can also use specific sizes like 14, 16 etc.
+
+plt.tight_layout()
 plt.grid(True)
+plt.savefig('periodic_1d_even_odd.png', format='png', dpi=300)
 plt.show()
+
