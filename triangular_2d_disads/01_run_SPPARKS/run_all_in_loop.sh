377c535ef19d95eba394865065d57c6068afb4c6
Patrick Park <3125patrick@gmail.com>
2024-04-23 10:02:48 -0700

    triangular 01

---
+++ triangular_2d_disads/01_run_SPPARKS/run_all_in_loop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -rf ../res
+rm -rf ../log
+
+for x in {3..5}; do
+    for y in {5..5}; do
+        size_x=$x
+        size_y=$y
+
+        ./run_multiple_with_input.sh $size_x $size_y
+        ../02_parse_log/loop_SPPARKS_logs.sh
+        python ../03_surface_coverage/compute_av_surface_coverage.py
+
+        if [ -d "../res" ]; then
+            mv ../res ../res_${size_x}_${size_y}
+        fi
+
+        if [ -d "../log" ]; then
+            mv ../log ../log_${size_x}_${size_y}
+        fi
+    done
+
+done
+
+python ../03_surface_coverage/plot_theta.py
